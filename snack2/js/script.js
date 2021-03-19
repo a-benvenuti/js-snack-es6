01879e2b1f115b2079d5e9c6336a59cb3d7cd3c4
Andrea <b.a.benvenuti3@gmail.com>
2021-03-19 16:59:35 +0100

    Completato snack2 ecmascript6

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -46,3 +46,43 @@ Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+//--------------------- LE FUNZIONI-----------------------------
+// restituisce un numero random tra due estremi compresi
+function random(min, max){
+  var numero = Math.floor(Math.random() * (max - min + 1)) + min;
+  return numero;
+}
+//--------------------fine DELLE FUNZIONI------------------------
+const teams = [
+ {
+   nome: "arsenal",
+   ptFatti: 0,
+   falliSub: 0,
+ },
+ {
+   nome: "new castle",
+   ptFatti: 0,
+   falliSub: 0,
+ },
+ {
+   nome: "leeds",
+   ptFatti: 0,
+   falliSub: 0,
+ },
+];
+for (let i = 0; i < teams.length; i++) {
+  teams[i].ptFatti = random(1, 114);
+  teams[i].falliSub = random(1, 40);
+}
+
+
+const statistiche = [];
+for (let i = 0; i < teams.length; i++) {
+  const {nome, falliSub} = teams[i];
+  statistiche.push({
+   nome,
+   falliSub,
+ });
+}
+
+console.log(statistiche);
