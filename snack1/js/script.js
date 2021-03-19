76f5c20ff140498b278e068f57cb1704c99155f8
Andrea <b.a.benvenuti3@gmail.com>
2021-03-19 16:05:45 +0100

    Completato snack1 ecmascript6

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -31,3 +31,28 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 */
+const bici = [
+  {
+    marca: 'graziella',
+    pesoKg: 4,
+  },
+  {
+    marca: 'bianchi',
+    pesoKg: 2,
+  },
+  {
+    marca: 'atala',
+    pesoKg: 3,
+  },
+];
+
+let biciLeggera = bici[0];
+for (let i = 0; i < bici.length; i++) {
+  if (bici[i].pesoKg < biciLeggera.pesoKg){
+    biciLeggera = bici[i];
+  }
+}
+
+const stampa = document.getElementById('stamp');
+const {marca, pesoKg} = biciLeggera;
+stampa.innerHTML = (`la bici più leggera è la: ${marca}, con un peso di: ${pesoKg} kg`);
